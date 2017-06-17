68ecfe1f4b07cf1f31a4cd30e99d1411ff25c088
SUPACHAI-PC\Supachai <me.supachai@gmail.com>
2017-06-17 14:12:48 +0700

    First Commit.

---
+++ Mail Test/Assets/Scripts/MailTester.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailTester : MonoBehaviour {
+
+    public void SendMail() {
+        Application.OpenURL("mailto:me.supachai@gmail.com?subject=You are a genius.&body=This is a test.");
+    }
+}
